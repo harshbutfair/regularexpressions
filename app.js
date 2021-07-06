fb8c3ba71ad75aa5a97d0da8692e46ddae02e6e9
harshbutfair <harsh.narayan@yahoo.com>
2021-07-06 21:53:02 +0530

    Shorthand character classes

--- app.js
+++ app.js
@@ -47,10 +47,42 @@ re = /h*llo/i;         // Matches any character 0 or more times
 re = /gre?a?y/;        // optional character
 re = /gre?a?y\?/;        // Escape character
 
+// Brackets [] - Character Sets
+re = /gr[ae]y/i;        // Must be an a or e
+re = /[GF]ray/i;        // Must be an G or F
+re = /[^GF]ray/;        // Match anything except a G or F
+re = /[A-Z]ray/;        // Match any uppercase letter
+re = /[a-z]ray/;        // Match any lowercase letter
+re = /[A-Za-z]ray/;        // Match any letter
+re = /[0-9]ray/;        // Match any digit
+re = /[0-9][0-9][0-9]ray/;        // Match any digit
+
+// Braces {} - Quantifiers
+re = /Hel{2}o/i;        // Must occur exactly {m} amount of times
+re = /Hel{2,4}o/i;      // Must occur exactly {m} to {n} amount of times
+re = /Hel{2,}o/i;       // Must occur at least {m} amount of times
+
+// Paranthesis () - Grouping
+re = /^([0-9]x){3}$/;
+
+// Shorthand character classes
+re = /\w/;      // Word character - alphanumeric or _
+re = /\w+/;     // + means on or more character
+re = /\W/;      // Non-word character
+re = /\d/;      // Match any digit
+re = /\d+/;     // Match any digit 0 or more times
+re = /\D/;      // Match any non-digit 
+re = /\s/;      // Match whitespace character
+re = /\S/;      // Match non-whitespace character
+re = /\bis\b/i; // Word boundary
+
+// Assertions
+re = /x(?=y)/;  // Match x only if followed by y
+re = /x(?!y)/;  // Match x only if NOT followed by y
 
 
 // String to match
-const str = 'gray?';
+const str = 'This island is beautifulx';
 
 // Log Results
 const result = re.exec(str);
